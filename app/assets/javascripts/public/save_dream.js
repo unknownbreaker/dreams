41a45affa63dd3a8e85a9fed5306e13c614644c5
unknownbreaker <rob.yang@gmail.com>
2014-12-18 18:29:03 -0500

    add explicit console logs to explain what is happening.

--- app/assets/javascripts/public/save_dream.js
+++ app/assets/javascripts/public/save_dream.js
@@ -6,8 +6,8 @@ SaveDream = {
       dataType: 'JSON',
       data: { dream: video_data },
       success: function(response) {
-        console.log("SUCCESS!")
-        console.log(response.key);
+        console.log("SUCCESS!");
+        console.log("This is the generated key that will be stored in another column in the dream model: ", response.key);
         // console.log(response.video_properties.dream);
       }
     })
