6e4d02ecc782864012ba3656a137c483e566938c
Michael Angelo <yamikamisama@gmail.com>
2015-06-06 11:47:34 -0400

    add gzip

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 
 module Dreams
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
